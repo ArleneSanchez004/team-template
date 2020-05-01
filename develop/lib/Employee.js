f9fef564b6eedf34a0cc17aa61e8f4704d6ea4e3
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-05-01 16:16:10 -0400

    created Employee class

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    findName(){
+        return this.name;
+    }
+    findId(){
+        return this.id;
+    }
+    findEmail(){
+        return this.email;
+    }
+    findRole(){
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
